061d9e9a66315b690228419b214fb652adb7d97c
risaltte <risaltte@gmail.com>
2022-05-05 15:48:08 -0400

    FormContent

--- src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -15,6 +15,7 @@ export function FeedbackTypeStep({ onFeedbackTypeChanged }: FeedbackTypeStepProp
 
                 <CloseButton />
             </header>
+            
             <div className="flex py-8 gap-2 w-full">  
                 {Object.entries(feedbackTypes).map(([key, value]) => {
                     return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,17 @@ module.exports = {
     extend: {
       colors: {
         brand: {
+          300: '#996dff',
           500: '#5257e6'
         }
+      },
+      borderRadius: {
+        md: '4px'
       }
     },
   },
   plugins: [
-    require('@tailwindcss/forms')
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar')
   ],
 }
