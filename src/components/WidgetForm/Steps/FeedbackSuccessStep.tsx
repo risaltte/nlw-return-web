bac06eb705bfd1ddfb0050222117eb4353ac46b9
Rafael Risalte <risaltte@gmail.com>
2022-05-05 23:16:01 -0400

    Finished web

--- src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -1,5 +1,31 @@
-export function FeedbackSuccessStep() {
+import { CloseButton } from "../../CloseButton";
+
+import emojiImageUrl from "../../../assets/img/emoji.svg";
+
+interface FeedbackSuccessStepProps {
+    onFeedbackRestartRequested: () => void;
+}
+
+export function FeedbackSuccessStep({ onFeedbackRestartRequested }: FeedbackSuccessStepProps) {
     return (
-        <div></div>
+        <>
+            <header>
+                <CloseButton />
+            </header>
+
+            <div className="flex flex-col items-center py-10 w-[304px]">
+                <img src={emojiImageUrl} alt="Imagem de um check de sucesso" />
+
+                <span className="text-xl mt-2">Agrademos o feedback!</span>
+
+                <button
+                    type="button"
+                    className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500  disabled:opacity-50 disabled:hover:bg-brand-500"
+                    onClick={onFeedbackRestartRequested}
+                >
+                    Quero enviar outro
+                </button>
+            </div>
+        </>
     );
 }
--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -5,6 +5,7 @@ import ideaImageUrl from '../../assets/img/idea.svg';
 import thoughtImageUrl from '../../assets/img/thought.svg';
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
 
 export const feedbackTypes = {
     BUG: {
@@ -34,22 +35,30 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+    const [feedbackSent, setFeedbackSent] = useState(false);
 
-    function handdleRestartFeedback() {
+    function handleRestartFeedback() {
         setFeedbackType(null);
+        setFeedbackSent(false);
     }
 
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            {!feedbackType ? (                
-                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
-            ): (
-                <FeedbackContentStep 
-                    feedbackType={feedbackType}
-                    onFeedbackRestartRequested={handdleRestartFeedback}
-                />
-            )
-            }
+            { feedbackSent ? (
+                <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback} />
+            ) : (
+                <>
+                    {!feedbackType ? (                
+                        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+                    ): (
+                        <FeedbackContentStep 
+                            feedbackType={feedbackType}
+                            onFeedbackRestartRequested={handleRestartFeedback}
+                            onFeedbackSent={() => setFeedbackSent(true)}
+                        />
+                     )}
+                </>
+            )}
 
             <footer>
                 Feito com ♥ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
